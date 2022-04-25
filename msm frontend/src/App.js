81d5a36ae44412b5c603d1e873cddfdeb4bab2af
Arnob2018331024 <mdfuadulislam0@gmail.com>
2022-04-25 17:53:38 +0600

    working...

--- msm frontend/src/App.js
+++ msm frontend/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import './components/Landing_page/Landing_page'
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+
 import LandingPage from './components/Landing_page/Landing_page';
 import ClientSearchPage from  './components/ClinetSearhPage/ClientSearchPage'
 import Login from './components/Login/Login'
@@ -14,20 +16,26 @@ function App() {
   const [user,setUser]=useState("f@gmail.com")
   const [searchedLocation,setSearchedLocation]=useState("PaikPara, B.Baria")
   return (
+    
     <div className="App">
       <div id="body">
-        {
-        //<LandingPage/>
-       //<Login/>
-       //<ClientSearchPage/>
-      //<UserProfile/>
-      //<ViewUserProfile user_email={user} searchedLocation={searchedLocation}/>
-        <SignUp1/>
-        //<SignUpAsWorker/>
-        //<SignUpAsClient/>
-        }
+      <Router>
+        <Routes>
+          
+        <Route path="/" exact element={<LandingPage/>}/>
+        <Route path="/login" exact element={<Login/>}/>
+        <Route path="/search" exact element={<ClientSearchPage/>}/>
+        <Route path="/profile" exact element={<UserProfile/>}/>
+        <Route path="/viewprofile" exact element={<ViewUserProfile user_email={user} searchedLocation={searchedLocation}/>}/>
+        <Route path="/signup1" exact element={<SignUp1/>}/>
+        <Route path="/signup2" exact element={<SignUpAsWorker/>}/>
+        <Route path="/signup3" exact element={<SignUpAsClient/>}/>
+          
+        </Routes>
+      </Router>
       </div>
     </div>
+
   );
 }
 
--- msm frontend/src/components/common/Title_bar.js
+++ msm frontend/src/components/common/Title_bar.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import './style.css'
 import LogoutMenu from "./LogoutMenu";
 
@@ -42,23 +43,23 @@ const TitleBar=({page,style,up})=>{
                 <div id="mms"><b>mms</b></div>
             </div>
            { page==="landing"?(<div id="buttons">
-                <div id="title_bar_login"><b>Log In</b></div>
-                <div id="title_bar_signup"><b>Sign Up</b></div> 
+                <Link to="/login" id="title_bar_login"><b>Log In</b></Link>
+                <Link to="/signup1" id="title_bar_signup"><b>Sign Up</b></Link> 
             </div>):null}
             
            { page==="clientPage"?(<div id="titlebarclientcontainer">
             <div id="clienttabs" class="tabcontainer">
-                <a href="" class="tab">profile</a>
-                <a href="" class="tab">find workers</a>
-                <a href="" class="tab">my hires</a>
+                <Link to="/profile" class="tab">profile</Link>
+                <Link to="/search" class="tab">find workers</Link>
+                <Link to="/hires" class="tab">my hires</Link>
             </div>
             <img src={up?up:localStorage.getItem("profile_pic")} id="titlebarimg" onClick={titleImgClick}/>
             </div>):null}
             { page==="workerPage"?(<div id="titlebarclientcontainer">
             <div id="clienttabs" class="tabcontainer">
-                <a href="" class="tab">profile</a>
-                <a href="" class="tab">my services</a>
-                <a href="" class="tab">my requests</a>
+                <Link to="/profile" class="tab">profile</Link>
+                <Link to="/services" class="tab">my services</Link>
+                <Link to="/requests" class="tab">my requests</Link>
             </div>
             <img src={up?up:localStorage.getItem("profile_pic")} id="titlebarimg" onClick={titleImgClick}/>
             </div>):null}
